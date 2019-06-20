44b424f2e588db98d887510dab1406ae8b7831a2
Djordje Stevanovic <EvonuX@users.noreply.github.com>
2019-06-20 21:04:36 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <main>
       <router-view/>
     </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
@@ -32,7 +35,7 @@ a {
   text-decoration: none;
 }
 
-img:not(.logoImg) {
+img:not(.logoImg, .poweredLogo) {
   width: 100%;
 }
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer>
+    <div class="copyright">Djordje Stevanovic &copy; {{ new Date().getFullYear() }}</div>
+    <div class="poweredBy">
+      <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
+        Powered by
+        <img src="@/assets/moviedbLogo.png" width="50px" class="poweredLogo">
+      </a>
+    </div>
+    <div class="github">
+      <a
+        href="https://github.com/EvonuX/Vue-Movies-v2"
+        target="_blank"
+        rel="noreferrer"
+      >View on GitHub</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style lang="scss">
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+  background-color: #212121;
+  color: #fff;
+
+  a {
+    color: #fff;
+  }
+
+  .poweredBy a {
+    color: #fff;
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-left: 15px;
+    }
+  }
+}
+</style>
